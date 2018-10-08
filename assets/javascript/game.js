9fb851dc4d4aab09fe58d50591c678d86fbd12ec
Irene Bogues <irenebogues@gmail.com>
2018-10-08 16:05:49 -0400

    stranger monday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,5 +2,31 @@
 // Always remember to code 1 piece at a time //
 // When in doubt, or this assignment gives you a terrible headache, go for a walk, exercise, or sleep //
 // Always remind yourself to breath and write to do's every after work so that you will remember what to do next and at the same try coding 1 piece at at time//
+/* The Characther of Stranger Things:
+1. Joyce
+2. Jim
+3. Mike
+4. Eleven
+5. Dustin
+6. Lucas
+7. Will
+*/
+
+
+
+
+//Array of blanks where gamers can type the guess letters //
+
+//Random names of characthers //
+
+
+//Game Loop//
+//Show Players their Progress//
+//Take input from the players//
+//Update answerArray and remaining letters for every correct guess//
+
+
+
+
 
 
