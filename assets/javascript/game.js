062c35b094b21c05ec6bc4fdc25d7ee5dd8f77dc
Irene Bogues <irenebogues@gmail.com>
2018-10-07 22:03:29 -0400

    strange sunday

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// Things to do for this word guessing game //
+// Always remember to code 1 piece at a time //
+// When in doubt, or this assignment gives you a terrible headache, go for a walk, exercise, or sleep //
+// Always remind yourself to breath and write to do's every after work so that you will remember what to do next and at the same try coding 1 piece at at time//
+
+
